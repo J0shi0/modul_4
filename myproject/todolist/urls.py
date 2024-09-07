b83ce5847b0a749a365148b63fc19db5aac2fb04
Fedor Dobrychev <dobrychev86@gmail.com>
2024-09-07 16:52:24 +0300

    Добавил сериализатор для Task, Comment, Tag. В views.py добавил viewset для всех моделей. Добавил эндпоинты для всех моделей.

--- myproject/todolist/urls.py
+++ myproject/todolist/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 from .views import base, about_us
+
+from rest_framework.routers import DefaultRouter
+from .views import TaskViewSet, TagViewSet, CommentListCreateAPIVIew, CommentRetrieveUpdateDestroyAPI
+
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet)
+router.register(r'tags', TagViewSet)
 
 urlpatterns = [
     path('', base, name='base'),
-    path('about_us', about_us, name='about_us')
-]
+    path('about_us', about_us, name='about_us'),
+    path('', include(router.urls)),
+    path(r'comments/', CommentListCreateAPIVIew.as_view()),
+    path(r'comments/<int:pk>/', CommentRetrieveUpdateDestroyAPI.as_view())
+    ]
--- myproject/todolist/views.py
+++ myproject/todolist/views.py
@@ -1,4 +1,8 @@
+from rest_framework import viewsets, generics, mixins
+
 from django.shortcuts import render
+from .models import Task, Comment, Tag
+from .serializers import TaskSerializer, CommentSerializer, TagSerializer
 
 
 def base(requests):
@@ -7,3 +11,23 @@ def base(requests):
 
 def about_us(requests):
     return render(requests, template_name='about_us.html')
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class CommentListCreateAPIVIew(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class CommentRetrieveUpdateDestroyAPI(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
